cbf776c7e252b2901da5c350b4d6521a63965c1a
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-05-08 23:48:17 +0530

    added: feature to get the searched hero and eder

--- index.js
+++ index.js
@@ -11,20 +11,10 @@ const herosSectionRowEl = document.querySelector(".heroes-section > .row");
 const heroSectionEl = document.querySelector(".hero-section");
 const heroSectionRowEl = document.querySelector(".hero-section > .row");
 
-console.log({
-  searchInputEl,
-  searchBtnEl,
-  formEl,
-  herosSectionEl,
-  herosSectionRowEl,
-  heroSectionEl,
-  heroSectionRowEl,
-});
 /**
  * to get the hero details on search
  */
 async function getHero() {
-  debugger;
   // hiding heros section
   herosSectionEl.classList.add("hide");
   heroSectionEl.classList.remove("hide");
@@ -37,19 +27,20 @@ async function getHero() {
         accept: "application/json",
       },
     };
-    // const response = await fetch(url, options);
-    // const data = await response.json();
+    const response = await fetch(url, options);
+    const data = await response.json();
+    console.log("path: ", data.data.results[0]);
     heroSectionRowEl.innerHTML = "";
     const heroContainer = document.createElement("div");
     heroContainer.className = "col";
     heroContainer.innerHTML = `
       <div class="card text-bg-dark">
-      <img src="https://images1.wionews.com/images/wion/900x1600/2023/4/12/1681294442560_MsMarvel.webp" class="card-img" alt="Ms Marvel" />
+      <img src="${data.data.results[0].thumbnail.path}.${data.data.results[0].thumbnail.extension}" class="card-img" alt="${data.data.results[0].name}" />
       <div
-      class="d-flex flex-column justify-content-end align-item-center text-center card-img-overlay movie-card-overlay"
+      class="d-flex flex-column justify-content-end align-item-center text-center card-img-overlay hero-card-overlay"
       >
-      <h5 class="card-title">Ms Marvel</h5>
-      <p class="card-text text-truncate text-wrap">Marvel Overview</p>
+      <h5 class="card-title">${data.data.results[0].name}</h5>
+      <p class="card-text text-wrap">${data.data.results[0].description}</p>
       <div class="d-flex justify-content-end">
       <button
       class="btn btn-light d-flex justify-content-center align-items-center rounded-pill w-25 like-btn"
